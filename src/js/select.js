b8700efc41b19a7860330a7f51726b6b94082bd0
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-12-20 12:25:42 +0100

    added icons, changed select

--- src/js/select.js
+++ src/js/select.js
@@ -1,55 +1,60 @@
 DB.init();
 
-/*DB.createUser({
- name: 'Peter',
- preferences: {
- 'Eat': true,
- 'Games': true
- }
- });
+//Todo: delete the ref to local storage
+localStorage.setItem('userid', '-KZQptxQ1phPaCxUMz0t');
 
- DB.createUser({
- name: 'Hans',
- preferences: {
- 'Eat': true,
- 'Drinks': true
- }
- });
+DB.getUserById(localStorage.getItem('userid'), function (user) {
 
- DB.createUser({
- name: 'Mary',
- preferences: {
- 'Eat': true,
- 'Netflix': true
- }
- });*/
 
-DB.getUserById('-KZN1oCPuUUP27dKQPMv', function(user) {
 
   let preferance = [];
+  let preferance_id = [];
+  let innertable = '';
 
-  console.log(user);
+  for (let pref of user.prefs) {
+    console.log(pref);
+    preferance.push(pref.name);
+    preferance_id.push(pref.id);
+    innertable += '<tr><td>'+pref.name+'</td> <td><button id="'+pref.id+'">Lets go!</button></td></tr>';
+
+    $('#select_secondary').on('click', 'button#'+pref.id, function () {
+      alert(pref.id)
+    })
+
+  }
+
+  let random = Math.floor(Math.random() * preferance.length);
+
+  console.log(random);
+  console.log(preferance);
+  console.log(preferance_id);
+
   $('#name').append(user.username);
+  $('#preference').append(preferance[random]);
+  if(preferance_id[random] == '-KZQpS72sYzV64D7Z3Xk' || preferance_id[random] == '-KZQpBcI-U4U0Jh7pcTk' ){
+    $('#preferencepic').append('<img src="assets/icons/'+preferance_id[random]+'.ico" />');
+  }else{
+    $('#preferencepic').append('<img src="assets/icons/'+preferance_id[random]+'.png" />');
+  }
 
-  DB.getAllPrefs(function (prefs) {
-    console.log(prefs);
-    for (let i in prefs) {
-      preferance.push([i, prefs[i].name]);
-    }
 
-    for (let i in user.prefs){
-      for (let x in preferance){
-        console.log(preferance[x][0]);
-        console.log(i);
-        if(preferance[x][0] == i){
-          $('#preference').append(preferance[x][1]);
-          break;
-        }
-      }
-    }
 
-  });
+  $('#select_button_true').click(function () {
+    localStorage.setItem('prefid', preferance_id[random]);
+    console.log(localStorage.getItem('prefid'));
+    window.location = 'map.html';
+  })
+
+  $('#select_button_false').click(function () {
+
+
+    $('#select_primary').hide();
+    $('#select_secondary').append('<table>'+innertable+'</table>')
+  })
+
 });
+
+
 
 
 
