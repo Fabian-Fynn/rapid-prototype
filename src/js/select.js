76a93f15fa9f8ba03a3cdd47c5a97593fb932f19
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-12-20 14:35:24 +0100

    changed select

--- src/js/select.js
+++ src/js/select.js
@@ -20,7 +20,7 @@ DB.getUserById(localStorage.getItem('userid'), function (user) {
     innertable += '<tr class="'+tablecolor+'" style="border-radius: 0;"><td style="border-radius: 0;">'+pref.name+'</td><td style="border-radius: 0;"><button class="btn green" id="'+pref.id+'"><i class="large material-icons">thumb_up</i></button></td></tr>';
 
     $('#select_secondary').on('click', 'button#'+pref.id, function () {
-      localStorage.setItem('prefid', preferance_id[random]);
+      localStorage.setItem('prefid', pref.id);
       window.location = 'map.html';
     });
 
