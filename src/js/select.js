c3096f25f134666945eae568b142b6bd721cd413
rudolfsonjunior <rudolfson.junior@gmail.com>
2016-12-20 13:47:23 +0100

    changed select added materialize

--- src/js/select.js
+++ src/js/select.js
@@ -1,47 +1,46 @@
 DB.init();
 
+
 //Todo: delete the ref to local storage
 localStorage.setItem('userid', '-KZQptxQ1phPaCxUMz0t');
 
 DB.getUserById(localStorage.getItem('userid'), function (user) {
 
-
-
   let preferance = [];
   let preferance_id = [];
   let innertable = '';
-
+  let count = 0;
+  let tablecolor = 'white black-text';
   for (let pref of user.prefs) {
-    console.log(pref);
+    if(count%2 == 0) tablecolor = 'grey darken-3 white-text';
+    else tablecolor = 'white black-text';
     preferance.push(pref.name);
     preferance_id.push(pref.id);
-    innertable += '<tr><td>'+pref.name+'</td> <td><button id="'+pref.id+'">Lets go!</button></td></tr>';
+    innertable += '<tr class="'+tablecolor+'" style="border-radius: 0;"><td style="border-radius: 0;">'+pref.name+'</td> <td style="border-radius: 0;"><button class="btn green" id="'+pref.id+'"><i class="large material-icons">thumb_up</i></button></td></tr>';
 
     $('#select_secondary').on('click', 'button#'+pref.id, function () {
-      alert(pref.id)
+      localStorage.setItem('prefid', preferance_id[random]);
+      window.location = 'map.html';
     })
+
+    count++;
 
   }
 
   let random = Math.floor(Math.random() * preferance.length);
 
-  console.log(random);
-  console.log(preferance);
-  console.log(preferance_id);
-
   $('#name').append(user.username);
   $('#preference').append(preferance[random]);
-  if(preferance_id[random] == '-KZQpS72sYzV64D7Z3Xk' || preferance_id[random] == '-KZQpBcI-U4U0Jh7pcTk' ){
-    $('#preferencepic').append('<img src="assets/icons/'+preferance_id[random]+'.ico" />');
-  }else{
-    $('#preferencepic').append('<img src="assets/icons/'+preferance_id[random]+'.png" />');
-  }
+  $('#preferencepic').append('<img src="assets/icons/'+preferance_id[random]+'.png" />');
 
+
+  DB.getUsersByPref('-KZQpN9qrdiEMESYmsHj', function(users) {
+    console.log('filtered users', users);
+  });
 
 
   $('#select_button_true').click(function () {
     localStorage.setItem('prefid', preferance_id[random]);
-    console.log(localStorage.getItem('prefid'));
     window.location = 'map.html';
   })
 
@@ -49,7 +48,7 @@ DB.getUserById(localStorage.getItem('userid'), function (user) {
 
 
     $('#select_primary').hide();
-    $('#select_secondary').append('<table>'+innertable+'</table>')
+    $('#select_secondary').append('<table class="centered">'+innertable+'</table>')
   })
 
 });
